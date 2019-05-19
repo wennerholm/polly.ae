a6a08004025650dc377c011ab8700191db482f93
dhar1993 <38814523+dhar1993@users.noreply.github.com>
2019-05-19 15:54:35 +0530

    updated text

--- src/App/Components/Navbar/Navbar.js
+++ src/App/Components/Navbar/Navbar.js
@@ -3,11 +3,12 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 // import Button from 'react-bootstrap/Button';
 import { LinkContainer } from "react-router-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class NavbarComponent extends Component {
   render() {
     return (
-      <Navbar bg="white" expand="lg" className="py-4">
+      <Navbar bg="white" expand="lg" className="py-4 shadow">
         <div className="container">
           <LinkContainer to="/">
             <Navbar.Brand href="#home">Polly</Navbar.Brand>
@@ -21,6 +22,10 @@ class NavbarComponent extends Component {
 
               <LinkContainer to="car-loan">
                 <Nav.Link>Car</Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="car-loan">
+                <Nav.Link><FontAwesomeIcon icon="mobile" /> Phone Number</Nav.Link>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
