6d7573e41847951cad489f777810865b76ceb8e0
mortuz <mortuzalam@gmail.com>
2019-05-19 16:57:44 +0530

    Updates

---
+++ precache-manifest.a20512f767f52ea0d9c017cb1c10b4c9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28dab157fc02dd6afbc95a695db8b450",
+    "url": "/polly/index.html"
+  },
+  {
+    "revision": "843171779445cda71faf",
+    "url": "/polly/static/css/main.7580f3ab.chunk.css"
+  },
+  {
+    "revision": "cf079090cc2c37b1bc84",
+    "url": "/polly/static/js/2.a2eecc93.chunk.js"
+  },
+  {
+    "revision": "843171779445cda71faf",
+    "url": "/polly/static/js/main.d265e940.chunk.js"
+  },
+  {
+    "revision": "8c13fe2f932280eb318f",
+    "url": "/polly/static/js/runtime~main.82899e88.js"
+  },
+  {
+    "revision": "ec75d7216eecb0f96e4622eaf5f90641",
+    "url": "/polly/static/media/backdropX.ec75d721.jpeg"
+  },
+  {
+    "revision": "7146cc474b0cce3dd2e931766413e581",
+    "url": "/polly/static/media/bank1.7146cc47.jpeg"
+  },
+  {
+    "revision": "4796f1875099eca5b3e5cc42ece7dc4e",
+    "url": "/polly/static/media/bank10.4796f187.jpg"
+  },
+  {
+    "revision": "84b4a15f9fc67e92687605dc0ba10188",
+    "url": "/polly/static/media/bank11.84b4a15f.jpg"
+  },
+  {
+    "revision": "df34800658ac5cd4a8b9e79dfbdc3699",
+    "url": "/polly/static/media/bank12.df348006.jpg"
+  },
+  {
+    "revision": "d3d4b148aba877d941c53bd124adf03c",
+    "url": "/polly/static/media/bank13.d3d4b148.jpg"
+  },
+  {
+    "revision": "0203eacd68b12af2a70dde78376cdfcb",
+    "url": "/polly/static/media/bank2.0203eacd.jpg"
+  },
+  {
+    "revision": "0c1b9f523f00186ab7ccb2d9106ef1a5",
+    "url": "/polly/static/media/bank3.0c1b9f52.jpg"
+  },
+  {
+    "revision": "bc5df5a4e64406464f4adb30d74317a8",
+    "url": "/polly/static/media/bank4.bc5df5a4.jpg"
+  },
+  {
+    "revision": "df6c5943cf0f05f1fdb2dff48593cdec",
+    "url": "/polly/static/media/bank5.df6c5943.jpg"
+  },
+  {
+    "revision": "0e92eb6461924ffe7c63aefba4571bd0",
+    "url": "/polly/static/media/bank6.0e92eb64.jpg"
+  },
+  {
+    "revision": "4b6cbd4a0fbc3926b93a70f97da9c5d2",
+    "url": "/polly/static/media/bank7.4b6cbd4a.jpg"
+  },
+  {
+    "revision": "be765b7ec6000b57bb965deccacfb253",
+    "url": "/polly/static/media/bank9.be765b7e.png"
+  },
+  {
+    "revision": "2e12b7e3addff58f7f84f3d2a786be50",
+    "url": "/polly/static/media/graphic.2e12b7e3.png"
+  },
+  {
+    "revision": "f6dc4d9c23a19e927c5ce5868bcdb5ea",
+    "url": "/polly/static/media/step-1.f6dc4d9c.svg"
+  },
+  {
+    "revision": "042c7aa90d9888eb0003b51b4895565c",
+    "url": "/polly/static/media/step-3.042c7aa9.svg"
+  },
+  {
+    "revision": "431b94469e2571156b70960e27880559",
+    "url": "/polly/static/media/step-4.431b9446.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.029ba21b05bae064fda6ee818fe465c4.js"
+  "/polly/precache-manifest.a20512f767f52ea0d9c017cb1c10b4c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/polly/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
